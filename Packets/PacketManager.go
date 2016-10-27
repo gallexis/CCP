4ee0220263ec76800c1ccbe5812a597dfc382313
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-10-27 17:32:07 +0200

    can now encode or decode packets
    (but the code is yet a mess..)

--- Packets/PacketManager.go
+++ Packets/PacketManager.go
@@ -1,44 +1,94 @@
 package Packets
 
 import (
-	"fmt"
+	//"fmt"
 	"bytes"
 	"encoding/binary"
 	payloads "CCP/Packets/Payloads"
+	"fmt"
 )
 
-type Header struct {
-	command_name [4]byte
+type packet_ struct{
+	hd header
+	pl []byte
+}
+
+type header struct {
+	command_name [5]byte
 	payload_length uint16
 }
 
-type Payload []byte
+type payload_ interface{}
+
+func Test(){
+
+	h1 := &header{}
+	h1.payload_length = uint16(12)
+	copy(h1.command_name[:], []byte("alert"))
 
 
-func (header *Header) PacketManager(packet []byte){
+	pl := []byte("123456789012")
 
-	buff_header := bytes.NewBuffer(packet)
+	fmt.Println(h1)
+	fmt.Println(pl)
 
-	payload, _ := header_reader(buff_header)
+	b := h1.header_writer()
+	fmt.Println(b)
 
-	if header_parsed.command_name == "alert"{
-		fmt.Println( payloads.V() )
-		fmt.Println( payload )
+	h2 := &header{}
+	h2.header_reader(bytes.NewBuffer(b))
+	fmt.Println(h2)
+	//-------
+
+	pck := &packet_{}
+	pck.hd = *h1
+	pck.pl = []byte("alexis")
+
+	fmt.Println(pck)
+	fmt.Println(pck.Encode_packet_to_binary())
+	fmt.Println(pck.Decode_binary_packet(pck.Encode_packet_to_binary()))
+
+
+}
+
+
+
+func (packet *packet_) Decode_binary_packet(pckt []byte) payload_{
+	header := &header{}
+	payload := ""
+
+	buff_packet := bytes.NewBuffer(pckt)
+	header.header_reader(buff_packet)
+
+	if bytes.HasSuffix(header.command_name[:], []byte("alert")){
+		payload = payloads.V()
+		//fmt.Println( payload )
+	}else {
+		payload = "err"
 	}
-
+	return payload
 }
 
-func (header *Header)header_reader(packet *bytes.Buffer) (*bytes.Buffer, error){
-	header.command_name = packet.Next(4)
-	header.payload_length = packet.Next(2)
 
-	return  packet, nil
+func (packet *packet_) Encode_packet_to_binary() []byte{
+	return append(packet.hd.header_writer()[:], packet.pl[:]...)
 }
 
-func (header *Header)header_writer() bytes.Buffer{
+func (header *header) header_reader(packet *bytes.Buffer) (error){
+
+	copy(header.command_name[:],packet.Next(5) )
+	header.payload_length = binary.LittleEndian.Uint16(packet.Next(2))
+
+	//binary.LittleEndian.PutUint16(header.payload_length, binary.LittleEndian.Uint16(packet.Next(2)))
+
+	return  nil
+}
+
+func (header *header) header_writer() []byte{
 	var bin_buf bytes.Buffer
-	binary.Write(&bin_buf, binary.BigEndian, header)
-	return bin_buf
+
+	binary.Write(&bin_buf, binary.LittleEndian, header)
+	return bin_buf.Bytes()
 }
 
 /*
--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
-import "fmt"
+
 import packets "CCP/Packets"
 
 
 // Car Communication Protocol
 func main() {
 
-	payload := []byte("blablablba blablbal done")
+packets.Test()
 
-	header := packets.NewHeader()
-
-	fmt.Println(payload)
-	fmt.Println(header)
 
 }
